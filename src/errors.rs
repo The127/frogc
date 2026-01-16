0955be7cabfa8d3d921bdf5b2b4dcbe141b35313
karo <karolin.kostial@gmail.com>
2026-01-16 16:00:08 +0100

    feat(errors): add `WrapError` type for improved error wrapping and propagation
    - Introduced `WrapError` struct to encapsulate inner errors with custom messages.
    - Implemented `Display` and `Error` traits for `WrapError` for
    
    Signed-off-by: karo <karolin.kostial@gmail.com>

--- src/errors.rs
+++ src/errors.rs
@@ -1,4 +1,29 @@
 #[derive(Debug)]
+pub struct WrapError {
+    inner: Box<dyn std::error::Error + Send + Sync>,
+    msg: String,
+}
+
+impl WrapError {
+    pub fn wrapper<E: std::error::Error + Send + Sync + 'static>(
+        msg: &str,
+    ) -> impl FnOnce(E) -> WrapError {
+        move |inner| WrapError {
+            inner: Box::new(inner),
+            msg: msg.to_string(),
+        }
+    }
+}
+
+impl std::fmt::Display for WrapError {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        write!(f, "{}: {}", self.msg, self.inner)
+    }
+}
+
+impl std::error::Error for WrapError {}
+
+#[derive(Debug)]
 pub enum ContainerError {
     AlreadyExists,
     NotFound,
