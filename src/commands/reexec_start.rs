f3f79fc6441868e7e7a5215010962b76914f601e
karo <karolin.kostial@gmail.com>
2026-01-16 16:00:31 +0100

    feat(reexec_start): implement enhanced container setup with mount and pivot logic
    - Added private mount propagation for container isolation.
    - Implemented `pivot_root` for root filesystem reassignment.
    - Integrated `proc` and `dev` filesystem mounts during container initialization.
    - Introduced working directory change and `umount` of old root.
    - Refactored `exec_container` logic for cleaner command execution.
    
    Signed-off-by: karo <karolin.kostial@gmail.com>

--- src/commands/reexec_start.rs
+++ src/commands/reexec_start.rs
@@ -1,43 +1,76 @@
-use std::ffi::CString;
-use std::fs;
-use std::os::unix::fs::chroot;
-use log::log;
-use nix::unistd::{chdir, execvp};
 use crate::context::FrogContext;
-use crate::errors::ContainerError;
+use crate::errors::{ContainerError, WrapError};
 use crate::types::ContainerSpec;
+use nix::mount::{MsFlags, mount, umount, umount2, MntFlags};
+use nix::unistd::{chdir, execvp, pivot_root};
+use std::ffi::CString;
+use std::fmt::format;
+use std::os::unix::fs::chroot;
 
 pub fn run(context: FrogContext, container_id: String) -> Result<(), ContainerError> {
-    fs::create_dir_all("./test").map_err(ContainerError::wrap)?;
-    Ok(())
-}
+    let state = context
+        .read_state(&container_id)
+        .map_err(WrapError::wrapper("reading container state"))
+        .map_err(ContainerError::wrap)?;
 
-unsafe fn child_main(id: String, spec: ContainerSpec) -> isize {
-    nix::mount::mount(
+    mount(
         None::<&str>,
         "/",
         None::<&str>,
-        nix::mount::MsFlags::MS_REC | nix::mount::MsFlags::MS_PRIVATE,
+        MsFlags::MS_PRIVATE | MsFlags::MS_REC,
         None::<&str>,
-    ).unwrap();
+    )
+        .map_err(WrapError::wrapper("making mounts private"))
+        .map_err(ContainerError::wrap)?;
 
-    nix::unistd::sethostname(id).unwrap();
+    pivot_root(state.spec.rootfs.as_str(), format!("{}/mnt", state.spec.rootfs).as_str())
+        .map_err(WrapError::wrapper("pivoting root"))
+        .map_err(ContainerError::wrap)?;
 
-    chroot(spec.rootfs).unwrap();
-    chdir("/").unwrap();
+    mount(
+        None::<&str>,
+        "/proc",
+        Some("proc"),
+        MsFlags::MS_NOSUID | MsFlags::MS_NOEXEC | MsFlags::MS_RELATIME | MsFlags::MS_NODEV,
+        None::<&str>,
+    )
+    .map_err(WrapError::wrapper("mounting proc"))
+    .map_err(ContainerError::wrap)?;
 
+    mount(
+        None::<&str>,
+        "/dev",
+        Some("devtmpfs"),
+        MsFlags::MS_NOSUID | MsFlags::MS_RELATIME,
+        None::<&str>,
+    )
+    .map_err(WrapError::wrapper("mounting dev"))
+    .map_err(ContainerError::wrap)?;
+
+    chdir(state.spec.work_dir.as_deref().unwrap_or("/"))
+        .map_err(WrapError::wrapper("changing working directory"))
+        .map_err(ContainerError::wrap)?;
+
+    umount2("/mnt", MntFlags::MNT_DETACH)
+        .map_err(WrapError::wrapper("umounting old root"))
+        .map_err(ContainerError::wrap)?;
+
+    nix::unistd::sethostname(container_id.clone())
+        .map_err(WrapError::wrapper("setting hostname"))
+        .unwrap();
+
+    exec_container(container_id.clone(), state.spec);
+
+    unreachable!()
+}
+
+fn exec_container(id: String, spec: ContainerSpec) {
     let cmd = CString::new(spec.cmd[0].as_str()).unwrap();
-    let args: Vec<CString> = spec.cmd.iter()
+    let args: Vec<CString> = spec
+        .cmd
+        .iter()
         .map(|s| CString::new(s.as_str()).unwrap())
         .collect();
 
-    let result = execvp(&cmd, &args);
-    match result {
-        Ok(_) => {
-            unreachable!()
-        }
-        Err(_) => {
-            1
-        }
-    }
+    execvp(&cmd, &args).unwrap();
 }
